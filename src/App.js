4a2f9a52b921beda39ce4645b4622a202d7bc006
Esha Patel <Patellesha11@gmail.com>
2025-03-27 13:21:23 +0300

    imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import FormInput from './FormInput';
+import Details from './Details';
 
 function App() {
   return (
     <div>
         <FormInput />
         <Details />
-        
+
     </div>
   );
 }
