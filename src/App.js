f951db4de945124a29cdf1c0ddecd05748b22a39
Esha Patel <Patellesha11@gmail.com>
2025-03-27 13:21:42 +0300

    imported

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import FormInput from './FormInput';
 import Details from './Details';
+import Registration from './Registration';
 
 function App() {
   return (
     <div>
         <FormInput />
         <Details />
+        <Registration />
 
     </div>
   );
