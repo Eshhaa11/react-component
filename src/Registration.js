71c17c7042adc735cd75878226e8193677b115af
Esha Patel <Patellesha11@gmail.com>
2025-03-27 13:06:21 +0300

    added button onclick

--- src/Registration.js
+++ src/Registration.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Registration () {
+
+    const handleButtonOnClick = (e) {
+        
+    }
     return (
         <div>
             <label>UserName:</label>
